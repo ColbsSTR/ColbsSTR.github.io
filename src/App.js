b3c9abfb82cf1bcb453622c45c0708de86a9f396
Colby Crowne <colbycrowne@Colbys-MBP.attlocal.net>
2022-01-24 00:38:32 -0600

    react meme generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Meme } from "./components/Meme.js";
+
+const objectToQueryParam = obj => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+  return "?" + params.join("&");
+};
 
 function App() {
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [meme, setMeme] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes").then(x =>
+      x.json().then(response => setTemplates(response.data.memes))
+    );
+  }, []);
+
+  if (meme) {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <img style={{ width: 400 }} src={meme} alt="custom meme" />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ textAlign: "center" }}>
+      {template && (
+        <form
+          onSubmit={async e => {
+            e.preventDefault();
+            // add logic to create meme from api
+            const params = {
+              template_id: template.id,
+              text0: topText,
+              text1: bottomText,
+              username: "Colby7432",
+              password: "abcdefg321"
+            };
+            const response = await fetch(
+              `https://api.imgflip.com/caption_image${objectToQueryParam(
+                params
+              )}`
+            );
+            const json = await response.json();
+            setMeme(json.data.url);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Meme template={template} />
+          <input
+            placeholder="top text"
+            value={topText}
+            onChange={e => setTopText(e.target.value)}
+          />
+          <input
+            placeholder="bottom text"
+            value={bottomText}
+            onChange={e => setBottomText(e.target.value)}
+          />
+          <button type="submit">create meme</button>
+        </form>
+      )}
+      {!template && (
+        <>
+          <h1>Pick a template</h1>
+          {templates.map(template => {
+            return (
+              <Meme
+                template={template}
+                onClick={() => {
+                  setTemplate(template);
+                }}
+              />
+            );
+          })}
+        </>
+      )}
     </div>
   );
 }
